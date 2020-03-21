3758b88e4b279548ed8aa8405e83d8257f815bbe
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2020-03-21 15:48:52 +0100

    Update package json

--- ui/screens/onboarding/index.js
+++ ui/screens/onboarding/index.js
@@ -17,19 +17,33 @@ import styles from "../../constants/Styles";
 import {MonoText} from "../../components/StyledText";
 
 const Stack = createStackNavigator();
+const INITIAL_ROUTE_NAME = 'OnboardingOne';
+const TITLE = 'ChainBreaker';
 
 export default function Onboarding () {
     return (
         <View style={styles.container}>
             {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
             <NavigationContainer>
-                <Stack.Navigator>
+                <Stack.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
                     <Stack.Screen
                         name={ "OnboardingOne" }
                         component={ OnboardingOne }
+                        options={{
+                            title: 'ChainBreaker'
+                        }}
                     />
+                    <Stack.Screen name={ "OnboardingTwo" } component={ OnboardingOne } />
+                    <Stack.Screen name={ "OnboardingThree" } component={ OnboardingOne } />
                 </Stack.Navigator>
             </NavigationContainer>
+            <View style={styles.tabBarInfoContainer}>
+                <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
+
+                <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+                    <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
+                </View>
+            </View>
         </View>
     );
 }
