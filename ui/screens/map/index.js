5bddd1548931aef68c6abb9333487310ab8306ec
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2020-03-22 21:53:39 +0100

    Add markers to map

--- ui/screens/map/index.js
+++ ui/screens/map/index.js
@@ -7,7 +7,59 @@ import MapView from "react-native-maps";
 import styles from "../../constants/Styles";
 
 
+function WarningMarker({ latitude, longitude }) {
+    let RADIUS = 100;
+
+    return (
+        <MapView.Circle
+            key = { (longitude + longitude).toString() }
+            center = { {latitude: latitude, longitude: longitude} }
+            radius = { RADIUS }
+            strokeWidth = { 1 }
+            strokeColor = { '#fdd004' }
+            fillColor = { 'rgba(253, 208, 4, 0.6)' }
+        />
+    );
+}
+
+function AlertMarker({ latitude, longitude }) {
+    let RADIUS = 150;
+
+    return (
+        <MapView.Circle
+            key = { (longitude + longitude).toString() }
+            center = { {latitude: latitude, longitude: longitude} }
+            radius = { RADIUS }
+            strokeWidth = { 1 }
+            strokeColor = { '#fdd004' }
+            fillColor = { 'rgba(237, 78, 68, 0.6)' }
+        />
+    );
+}
+
 export default function MapScreen() {
+
+    let latitude =  37.78825;
+    let longitude =  -122.4324;
+    let latitudeDelta =  0.0922;
+    let longitudeDelta =  0.0421;
+
+    let warningLatLongList = [
+        { latitude: 37.78825, longitude: -122.4324 },
+        { latitude: 37.78745, longitude: -122.4324 },
+        { latitude: 37.78265, longitude: -122.4324 },
+        { latitude: 37.77705, longitude: -122.4114 },
+        { latitude: 37.78895, longitude: -122.4314 },
+    ];
+
+    let alertLatLongList = [
+        { latitude: 37.79825, longitude: -122.4324 },
+        { latitude: 37.70745, longitude: -122.4924 },
+        { latitude: 37.78265, longitude: -122.4624 },
+        { latitude: 37.78705, longitude: -122.3114 },
+        { latitude: 37.78895, longitude: -122.4314 },
+    ];
+
 
     return (
         <View style={styles.container}>
@@ -15,12 +67,17 @@ export default function MapScreen() {
                 <View  style={mapStyle.mapContainer}>
                     <MapView style={mapStyle.map}
                              initialRegion={{
-                                 latitude: 37.78825,
-                                 longitude: -122.4324,
-                                 latitudeDelta: 0.0922,
-                                 longitudeDelta: 0.0421,
-                             }}
-                    />
+                                 latitude: latitude,
+                                 longitude: longitude,
+                                 latitudeDelta: latitudeDelta,
+                                 longitudeDelta: longitudeDelta,
+                             }}>
+                        {/*<WarningMarker latitude={latitude} longitude={longitude}/>*/}
+                        {warningLatLongList.map(coords => <WarningMarker latitude={coords.latitude} longitude={coords.longitude}/>)}
+                        {alertLatLongList.map(coords => <AlertMarker latitude={coords.latitude} longitude={coords.longitude}/>)}
+
+                    </MapView>
+
                 </View>
             {/*</ScrollView>*/}
         </View>
