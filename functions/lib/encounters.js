c3363d04327dd746e97523958aeae00e8805ddca
Niklas Gögge <n.goeggi@gmail.com>
2020-03-27 02:29:53 +0100

    Add http backend api
    GET /encounters?pids=<pid1>,...
    Retrieves all encounters matching one of the supplied pids
    
    POST /encounters
    Upload encounters to the database

---
+++ functions/lib/encounters.js
@@ -0,0 +1,63 @@
+const admin = require('./admin');
+const ENCOUNTERS_COLLECTION = 'encounters';
+// TODO: set to correct length
+const PID_LENGTH = 16
+
+// checks if an encounter has the required fields
+function validEncounter(encounter) {
+	const encounterFields = {
+		duration: (val) => typeof val === 'number',
+		pid: (val) => typeof val === 'string' && val.length <= PID_LENGTH,
+		min_distance: (val) => typeof val === 'number',
+		timestamp: (val) => typeof val === 'number',
+	};
+
+	const encounterKeys = Object.keys(encounter);
+	const filteredKeys = encounterKeys.filter(field => encounterFields[field](encounter[field]));
+	return filteredKeys.length === encounterKeys.length;
+}
+
+function snapshotToData(snap) {
+	return snap.docs.map(doc => doc.data())
+}
+
+function flatten(array) {
+	return array.reduce((acc, val) => acc.concat(val), [])
+}
+
+/**
+ * lookupN fetches all encouters that match one of the supplied pids.
+ * 
+ * TODO: set a limit to the number of supplied pids.
+ */
+exports.lookupN = function (pids) {
+	const encounterQueries = pids.map(pid => exports.lookup(pid))
+	return Promise.all(encounterQueries).then(flatten)
+}
+
+exports.lookup = function (pid) {
+	return admin.firestore().collection(ENCOUNTERS_COLLECTION)
+		.where('pid', '==', pid)
+		.orderBy('timestamp')
+		.get()
+		.then(snapshotToData);
+}
+
+exports.store = function (encounters) {
+	if (!encounters) {
+		return Promise.reject(new Error("No encounters supplied"));
+	}
+
+	if (encounters.filter(validEncounter) < encounters.length) {
+		return Promise.reject(new Error("Invalid encounter supplied"));
+	}
+
+	const batch = admin.firestore().batch();
+	encounters.forEach(encounter => {
+		batch.set(
+			admin.firestore().collection(ENCOUNTERS_COLLECTION).doc(),
+			encounter
+		)
+	});
+	return batch.commit()
+}
