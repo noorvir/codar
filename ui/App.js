e0fa2e4f25d55deb72fd761a31465174513261a2
Lukas Rambold <lukas@rambold.io>
2020-03-29 18:44:18 +0200

    removed expo dependencies

--- ui/App.js
+++ ui/App.js
@@ -5,25 +5,24 @@ import {
   Platform,
   StatusBar,
   StyleSheet,
+  AsyncStorage,
   View,
   AppRegistry,
+  NativeModules,
   I18nManager
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { SplashScreen } from 'expo';
-
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
 
-import * as Localization from 'expo-localization';
 import i18n from 'i18n-js';
 
 import Onboarding from 'react-native-onboarding-swiper';
 
-import * as Font from 'expo-font';
+// import * as Font from 'expo-font';
 
 import {
   setCustomView,
@@ -35,7 +34,11 @@ i18n.translations = {
   de: require("./translations/de.json"),
 };
 // Set the locale once at the beginning of your app.
-i18n.locale = Localization.locale;
+// TODO: set locale dynamically
+i18n.locale = Platform.OS === 'ios'
+        ? NativeModules.SettingsManager.settings.AppleLocale ||
+          NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+        : NativeModules.I18nManager.localeIdentifier;;
 i18n.fallbacks = true;
 
 // Setting a default background color for all View components.
@@ -49,23 +52,39 @@ export default function App(props) {
   const containerRef = React.useRef();
   const { getInitialState } = useLinking(containerRef);
 
+  const finishOnboarding = () => {
+    setIsInitialized(true)
+    const _storeData = async () => {
+        try {
+            await AsyncStorage.setItem('onboardingFinsihed', 'true');
+        } catch (error) {
+            // Error saving data
+            console.warn(error);
+            console.warn("Couldn't store that Onboarding finished.")
+        }
+    }
+    _storeData()
+  }
+
   // Load any resources or data that we need prior to rendering the app
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
-        SplashScreen.preventAutoHide();
+        // SplashScreen.preventAutoHide();
 
         // Load our initial navigation state
         setInitialNavigationState(await getInitialState());
 
+        setIsInitialized(await AsyncStorage.getItem('onboardingFinsihed'));
+
         // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'ubuntu': require('./assets/fonts/Ubuntu-R.ttf'),
-        });
+        // await Font.loadAsync({
+        //   ...Ionicons.font,
+        //   'ubuntu': require('./assets/fonts/Ubuntu-R.ttf'),
+        // });
         setCustomText({
           style: {
-            fontFamily: 'ubuntu' // light gray
+            fontFamily: 'Ubuntu Regular' // light gray
           }
         });
       } catch (e) {
@@ -73,7 +92,7 @@ export default function App(props) {
         console.warn(e);
       } finally {
         setLoadingComplete(true);
-        SplashScreen.hide();
+        // SplashScreen.hide();
       }
     }
 
@@ -110,7 +129,7 @@ export default function App(props) {
                 subtitle: i18n.t('onboarding.step_4_caption'),
               },
             ]}
-            onDone={() => setIsInitialized(true)}
+            onDone={finishOnboarding}
           />
           </View>
       )
