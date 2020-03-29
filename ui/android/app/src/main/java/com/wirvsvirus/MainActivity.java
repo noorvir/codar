36c73abc4ac6118414da1386ea2b0e25fea4f9a4
Nelson Osacky <nelson.osacky@soundcloud.com>
2020-03-29 15:40:21 +0200

    Add permissions for bluetooth and location.

--- ui/android/app/src/main/java/com/wirvsvirus/MainActivity.java
+++ ui/android/app/src/main/java/com/wirvsvirus/MainActivity.java
@@ -1,9 +1,20 @@
 package com.wirvsvirus;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends ReactActivity {
 
@@ -24,5 +35,55 @@ public class MainActivity extends ReactActivity {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
         };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initBluetoothAdapter();
+        initLocationManager();
+    }
+
+    /**
+     * Initializes the BluetoothAdapter. Manifest file is already setup to allow bluetooth access.
+     * The user will be asked to enable bluetooth if it is turned off
+     */
+    private void initBluetoothAdapter() {
+        BluetoothAdapter bluetoothAdapter = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE)).getAdapter();
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
+    }
+
+    private void initLocationManager() {
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        );
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            ) {
+                Toast.makeText(
+                        this,
+                        "The permission to get BLE location data is required",
+                        Toast.LENGTH_SHORT
+                ).show();
+            } else {
+                requestPermissions(
+                        new String[]{
+                                Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                        }, 1
+                );
+            }
+        } else {
+            // Toast.makeText(this, "Location permissions already granted", Toast.LENGTH_SHORT).show()
+        }
     }
 }
