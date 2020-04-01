9ee815bf1f19c0bbd4183c7db70b717fa114d6fc
Niklas Gögge <n.goeggi@gmail.com>
2020-04-01 19:51:02 +0200

    Use spaces

--- app/components/InfectiousEncounterFetcher.js
+++ app/components/InfectiousEncounterFetcher.js
@@ -8,45 +8,54 @@ const STORAGE_FETCH_INTERVAL = 5000; // 5 sec.
 
 export const InfectiousEncounterContext = createContext([]);
 
+/**
+ * InfectiousEncounterContext provider.
+ * 
+ * Usage:
+ *  <InfectiousEncounterFetcher>
+ *    <ComponentThatUsesThe_InfectiousEncounterContext />
+ *  </InfectiousEncounterFetcher>
+ */
 export default function InfectiousEncounterFetcher({ children }) {
-	const [mounted, setMounted] = useState(false);
-	const [encounters, setEncounters] = useState([]);
+  const [mounted, setMounted] = useState(false);
+  const [encounters, setEncounters] = useState([]);
 
-	useEffect(() => {
-		if (!mounted) {
-			// register background task that fetches infectious matching encounters 
-			// writes results to AsyncStorage with key: STORAGE_KEY
-			// TODO: send push notifications
-			BackgroundFetch.configure({
-				minimumFetchInterval: 15,
-				requiredNetworkType: BackgroundFetch.NETWORK_TYPE_ANY,
-			}, async (taskId) => {
-				checkPublicEncounters()
-					.then(encounters => AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({ encounters })))
-					.then(() => BackgroundFetch.finish(taskId))
-					.catch(error => {
-						console.log(error);
-						BackgroundFetch.finish(taskId);
-					});
-			});
-		}
+  useEffect(() => {
+    if (!mounted) {
+      // register background task that fetches infectious matching encounters 
+      // writes results to AsyncStorage with key: STORAGE_KEY
+      // TODO: send push notifications
+      BackgroundFetch.configure({
+        minimumFetchInterval: 15,
+        requiredNetworkType: BackgroundFetch.NETWORK_TYPE_ANY,
+      }, async (taskId) => {
+        checkPublicEncounters()
+          .then(encounters => AsyncStorage.setItem(STORAGE_KEY, JSON.stringify({ encounters })))
+          .then(() => BackgroundFetch.finish(taskId))
+          .catch(error => {
+            console.log(error);
+            BackgroundFetch.finish(taskId);
+          });
+      });
+    }
 
-		const intervalId = setInterval(() => {
-			AsyncStorage.getItem(STORAGE_KEY).then(value => {
-				if (value) {
-					setEncounters(JSON.parse(value).encounters);
-				}
-			})
-		}, STORAGE_FETCH_INTERVAL);
+    // Updates the `encounters` state every `STORAGE_FETCH_INTERVAL` interval.
+    const intervalId = setInterval(() => {
+      AsyncStorage.getItem(STORAGE_KEY).then(value => {
+        if (value) {
+          setEncounters(JSON.parse(value).encounters);
+        }
+      })
+    }, STORAGE_FETCH_INTERVAL);
 
-		setMounted(true);
-		return () => clearInterval(intervalId);
-	});
+    setMounted(true);
+    return () => clearInterval(intervalId);
+  });
 
-	return (
-		<InfectiousEncounterContext.Provider value={encounters}>
-			{children}
-		</InfectiousEncounterContext.Provider>
-	);
+  return (
+    <InfectiousEncounterContext.Provider value={encounters}>
+      {children}
+    </InfectiousEncounterContext.Provider>
+  );
 }
 
